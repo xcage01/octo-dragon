89f9881291f620d0f9f72f8d99f28e6a44416613
xander cage <xcage01@hotmail.com>
2016-07-01 22:57:11 +0530

    Testing out with two apps

--- src/dragon.cc
+++ src/dragon.cc
@@ -37,6 +37,42 @@ namespace testApp
 }
 
 
+namespace testApp2
+{
+  const char * appName = "testApp2";  
+  urlDispatch * urls = new urlDispatch;
+
+  std::list<url *> urlPattern;
+
+  request * hello(request * req)
+  {
+    HttpResponse("Testing if macros are good")
+  }
+
+  request * hello2(request * req)
+  {
+    HttpResponse("testing another url")
+  }
+  
+  request * notFound(request * req)
+  {
+    HttpResponse(req->url)
+  }
+
+  appMeta * init()
+  {
+    urlPattern.push_back(new url("/",hello));
+    urlPattern.push_back(new url("/h",hello2));
+    appMeta * application = new appMeta;
+    application -> appName = appName;
+    application -> urls = urlPattern;
+    return application;
+  }
+
+
+}
+
+
 
 
 
@@ -44,6 +80,7 @@ int main()
 {
   HttpServer * baseServer = new HttpServer();
   baseServer -> registerApp("/testApp",testApp::init);
+  baseServer -> registerApp("/testApp2",testApp2::init);
   baseServer -> notFound = testApp::notFound;
   baseServer -> serve();
 }
