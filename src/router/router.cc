fe4f809e4150188e8966cec647d4f9e4659ad6d0
xander cage <xcage01@hotmail.com>
2016-07-07 12:24:12 +0530

    working router calls with regex

--- src/router/router.cc
+++ src/router/router.cc
@@ -13,13 +13,27 @@ router::~router()
 void router::add(std::string baseUri,url urlList)
 {
         router::baseController.insert(std::pair<std::string,url>(baseUri,urlList));
-        std::cout << "Size of urlPattern init "<< router::baseController.size()<<std::endl;
 }
 
 void router::route(httpRequest* request)
 {
-        std::cout << "Size of urlPattern end "<< router::baseController.size()<<std::endl;
-        request->reply("Simple router case",200);
+        bool found = false;
+        for (const auto& appUrlConf:router::baseController )
+        {
+                std::string appBaseUri = appUrlConf.first;
+                for (const auto& appUri:appUrlConf.second)
+                {
+                        std::string regexUri;
+                        regexUri.append(appUrlConf.first).append(appUri.first);
+                        if(std::regex_match(request->url,std::regex(regexUri)))
+                        {
+                                appUri.second(request);
+                                found = true;
+                        }
+                }
+        }
+        if(!found)
+                request->reply("Page not found",404);
 }
 
 // Defination of all static members of class
